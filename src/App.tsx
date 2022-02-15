f0afbc141f4a8526c6d18a92cd1e7dbf8c87d53a
milkeddy1 <81408367+milkeddy1@users.noreply.github.com>
2022-02-15 15:34:51 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import Requirement from "./Requirement";
+
+import mockData from "../mock"; // 請使用此資料
+
+function App() {
+  return <div>123</div>;
+}
+
+export default App;
